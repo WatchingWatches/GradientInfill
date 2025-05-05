79c11b29749f46d844e8023b56f6038df1dd220f
Benedikt Jansson <bjansson.bj40@gmail.com>
2025-05-05 14:08:52 +0200

    fixed bug in volumetric flow, added near wall/collinear detection

--- Universal/universal_addGradientinfill.py
+++ Universal/universal_addGradientinfill.py
@@ -15,7 +15,7 @@ from enum import Enum
 from typing import List, Tuple
 import traceback
 import time
-from math import pi
+from math import pi, degrees, acos
 
 
 __version__ = '1.0'
@@ -40,7 +40,7 @@ class Slicer(Enum):
     ORCA = 1 # if you use a bambulab printer choose Bambu
     BAMBU = 2
     PRUSA = 3
-    CURA = 4 
+    CURA = 4 # untested
 
 
 Point2D = namedtuple('Point2D', 'x y')
@@ -49,23 +49,21 @@ Segment = namedtuple('Segment', 'point1 point2')
 # EDIT this section for your creation parameters
 # if the filenames have the same name the original file will be overwritten
 # names only used if run in IDE
-INPUT_FILE_NAME: str = r"C:\Users\bjans\Downloads\Torus_PLA_1h20m.gcode"
+INPUT_FILE_NAME: str = r"Universal\bunny_orca.gcode"
 OUTPUT_FILE_NAME: str = r"C:\Users\bjans\Downloads\gradinfilltest_res.gcode"
 
 # Warning there is just one file as output from the slicer, which means you can't compare it to the original
 dialog_in_slicer: bool = True # use different parameters inside of the slicer via dialog else the following values are used
-REMOVE_SLICER_INFO: bool = True # remove first line with slicer information for realistic gcode preview only for prusa, orca slicer
+REMOVE_SLICER_INFO: bool = False # remove first line with slicer information for realistic gcode preview only for prusa, orca slicer
 D_F: float = 1.75  # diameter of the filament in mm
 # this setting is only relevant for SMALL_SEGMENTS infill when disabled the infill outside of the GRADIENT_THICKNESS isn't changed
 THIN_INNER_CORE: bool = True 
-
-INFILL_TYPE: InfillType = InfillType.SMALL_SEGMENTS
 # automatically search for the slicer with: Slicer.SEARCH
 Slicer_Type: Slicer = Slicer.SEARCH # if manually assigned and you use a bambulab printer with Orca slicer choose Bambu!
 
-MAX_FLOW: float = 250.0  # maximum extrusion flow
+MAX_FLOW: float = 220.0  # maximum extrusion flow
 MIN_FLOW: float = 70.0  # minimum extrusion flow
-GRADIENT_THICKNESS: float = 8.0  # thickness of the gradient (max to min) in mm
+GRADIENT_THICKNESS: float = 12.0  # thickness of the gradient (max to min) in mm
 GRADIENT_DISCRETIZATION: float = 4.0  # only applicable for linear infills; number of segments within the
 # gradient(segmentLength=gradientThickness / gradientDiscretization); use sensible values to not overload the printer
 
@@ -123,8 +121,8 @@ def get_points_distance(point1: Point2D, point2: Point2D) -> float:
     """
     return ((point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2) ** 0.5
 
-# fine
-def min_distance_from_segment(segment: Segment, segments: List[Segment]) -> float:
+# changed
+def min_distance_from_segment(segment: Segment, segments: List[Segment], return_seg: bool = False) -> float:
     """Calculate the minimum distance from the midpoint of ``segment`` to the nearest segment in ``segments``.
 
     Args:
@@ -135,8 +133,12 @@ def min_distance_from_segment(segment: Segment, segments: List[Segment]) -> floa
         float: the smallest distance from the midpoint of ``segment`` to the nearest segment in the list
     """
     middlePoint = Point2D((segment.point1.x + segment.point2.x) / 2, (segment.point1.y + segment.point2.y) / 2)
-
-    return min(dist(s, middlePoint) for s in segments)
+    min_segment = min(segments, key=lambda s: dist(s, middlePoint))
+    min_value = dist(min_segment, middlePoint)
+    if return_seg:
+        return min_value, min_segment
+    else:
+        return min_value
 
 # use re.compile to make it quicker
 prog_searchX = re.compile(r"X(\d*\.?\d*)")
@@ -242,7 +244,7 @@ def is_begin_inner_wall_line(line: str) -> bool:
 
 
 # changed
-def is_end_inner_wall_line(line: str) -> bool:
+def is_end_inner_wall_line(line: str) -> bool: #TODO delete?
     """Check if current line is the start of an outer wall section.
 
     Args:
@@ -325,13 +327,40 @@ def control_flow(hotend_max_flow: float, extrusionLength:float, distance:float, 
     F = round((hotend_max_flow*distance*60*4)/(extrusionLength*(d_f**2)*pi), 3)
     return "G1 F{}\n".format(F) 
 
+def is_collinear(wall:Segment, p_0:Point2D, p_1:Point2D)->bool:
+    """ Check if the infill segment is nearly collinear with the wall segment.
+    Args:
+        wall (Segment): The wall segment represented by two points (point1 and point2).
+        p_0 (Point2D): The starting point of the infill segment.
+        p_1 (Point2D): The ending point of the infill segment.
+    Returns:
+        bool: True if the infill segment is collinear with the wall segment
+    """
+    def clamp(value, min_value, max_value)-> float:
+        return max(min(value, max_value), min_value)
+    try:
+        v_wall = Point2D(wall.point2.x - wall.point1.x, wall.point2.y - wall.point1.y)
+        norm_wall = Point2D(*(1 / get_points_distance(wall.point1, wall.point2) * v for v in (v_wall.x, v_wall.y)))
+        v_infill = Point2D(p_1.x - p_0.x, p_1.y - p_0.y)
+        norm_infill = Point2D(*((1 / get_points_distance(p_0, p_1)) * v for v in v_infill))
+        neg_norm_infill = Point2D(*((-1) * v for v in norm_infill))
+        
+        dot_product = norm_wall.x * norm_infill.x + norm_wall.y * norm_infill.y
+        neg_dot_product = norm_wall.x * neg_norm_infill.x + norm_wall.y * neg_norm_infill.y
+        # add boundries 
+        dot_product = clamp(dot_product, -1, 1)
+        neg_dot_product = clamp(neg_dot_product, -1, 1)
+
+        angle = min(abs(degrees(acos(dot_product))), abs(degrees(acos(neg_dot_product))))
+    except ZeroDivisionError:
+        angle = 0
+    return angle < 15
 
 lines = []
 # change to use search patterns instead of finding elements in string
 def process_gcode(
     input_file_name: str,
     output_file_name: str,
-    infill_type: InfillType,
     max_flow: float,
     min_flow: float,
     gradient_thickness: float,
@@ -352,17 +381,30 @@ def process_gcode(
     ignore_pos = True
     is_old_speed = False
     currentSection = Section.NOTHING
-    lastPosition = Point2D(-float('inf'), -float('inf'))# set infinate start point
+    lastPosition = Point2D(-float('inf'), -float('inf')) # set infinate start point
     gradientDiscretizationLength = gradient_thickness / gradient_discretization
+    small_segments_infill_type = ['gyroid', 'honeycomb', '3dhoneycomb'] # list of all small segments infill types
 
     with open(input_file_name, "r") as gcodeFile:        
         gcode = gcodeFile.readlines()
-        # find the volumetric flow limit of the filament from slicer settings (probaly not compatible with cura) #TODO automatically find infill type
+        found_flow = False
+        found_infill_type = False
+        # find the volumetric flow limit of the filament from slicer settings (probaly not compatible with cura) and reversing isn't optimal for bambu
         for line in reversed(gcode):
             if line.startswith('; filament_max_volumetric_speed ='):
-                hotend_max_flow = min(map(float, line.split('=')[-1].split(',')))# different filaments are sperated by , minimum is used
+                hotend_max_flow = min(map(float, line.split('=')[-1].split(','))) # different filaments are sperated by , minimum is used
                 print(f'Maximum flow is: {hotend_max_flow} [mm^3/s]')
-                break
+                found_flow = True
+            if line.startswith('; fill_pattern = ') or line.startswith('; sparse_infill_pattern = '):
+                infill_type = line.split('=')[-1].strip().split()[0]
+                if infill_type in small_segments_infill_type:
+                    infill_type = InfillType.SMALL_SEGMENTS
+                else:
+                    infill_type = InfillType.LINEAR
+                found_infill_type = True
+            if all((found_infill_type, found_flow)):
+                print(infill_type)
+                break # task finished
 
         for currentLine in gcode:
             # find the slicer automatically
@@ -392,6 +434,12 @@ def process_gcode(
                 Slicer_Type = Slicer.BAMBU
                 prog_type = re.compile(r'^; FEATURE:')
 
+            # get line width to determine critical zone next to wall
+            if currentLine.startswith("; infill extrusion width = ") or currentLine.startswith("; sparse_infill_line_width = "):
+                infill_linewidth = float(currentLine.split("=")[-1].split("mm")[0])
+            if currentLine.startswith("; perimeters extrusion width = ") or currentLine.startswith("; inner_wall_line_width = "):
+                inner_wall_linewidth = float(currentLine.split("=")[-1].split("mm")[0])
+
             writtenToFile = 0
                 
             if is_begin_layer_line(currentLine):
@@ -403,17 +451,12 @@ def process_gcode(
                     raise SyntaxError("Slicer not found.")
                     
                 # ignore start or end gcode
-                if is_start_gcode(currentLine):
-                    ignore_pos = True
-                else:
-                    ignore_pos = False
+                ignore_pos = is_start_gcode(currentLine)
 
                 if is_begin_inner_wall_line(currentLine):
                     currentSection = Section.INNER_WALL
-
                 elif is_begin_infill_segment_line(currentLine):
                     currentSection = Section.INFILL
-                
                 # irrelevent type, this was in the cura version searching for ; at the end
                 else:
                     currentSection = Section.NOTHING
@@ -458,8 +501,8 @@ def process_gcode(
                         # calculate original infill flow once per layer TODO might lead to issue with multiple objects with different settings
                         if infill_begin:
                             infill_flow = (float(infill_speed)*(d_f**2)*pi*extrusionLength) / (4*segmentLength*60)
-                        else:
                             infill_begin = False
+
                         if segmentSteps >= 2:
                             for _ in range(int(segmentSteps)):
                                 segmentEnd = Point2D(
@@ -529,11 +572,14 @@ def process_gcode(
                             lines.append(outPutLine)
                         writtenToFile = 1
 
-                    # gyroid or honeycomb
+                    # gyroid or 3d/honeycomb
                     if infill_type == InfillType.SMALL_SEGMENTS:
-                        shortestDistance = min_distance_from_segment(
-                            Segment(lastPosition, currentPosition), perimeterSegments
+                        seg_info = min_distance_from_segment(
+                            Segment(lastPosition, currentPosition), perimeterSegments, return_seg=True
                         )
+                        shortestDistance = seg_info[0]
+                        min_seg = seg_info[1]
+                        collinear = is_collinear(min_seg, lastPosition, currentPosition)
 
                         outPutLine = ""
                         if shortestDistance < gradient_thickness:
@@ -542,26 +588,30 @@ def process_gcode(
                                     flow_factor = mapRange(
                                         (0, gradient_thickness), (max_flow / 100, min_flow / 100), shortestDistance
                                     )
+                                    # prevent unclean extrusion near the wall
+                                    critical_distance = (infill_linewidth + inner_wall_linewidth) * 1.4 / 2
+                                    if shortestDistance <= critical_distance and collinear:
+                                        flow_factor = 1
                                     newE = float(element[1:]) * flow_factor
                                     # calculate original infill flow once per layer
                                     if infill_begin:
                                         segmentLength = get_points_distance(lastPosition, currentPosition)
                                         infill_flow = (float(infill_speed)*(d_f**2)*pi*float(element[1:])) / (4*segmentLength*60)
+                                        infill_begin = False
 
                                         if infill_flow > hotend_max_flow + 0.5: #TODO delete? will trigger if wrong max flow is set only happens with two filaments
                                             print('Your infill flow is higher, than the hotend limit in the script!')
                                             print('Please adjust either your slicer or script settings')
                                             print('Slicer Infill Flow:', infill_flow, 'Script Hotend Max Flow:', hotend_max_flow, '[mm^3/s]')
                                             input()
-                                    else:
-                                        infill_begin = False
+                                        
                                     outPutLine = outPutLine + "E" + str(round(newE, 5))
                                 else:
                                     outPutLine = outPutLine + element + " "
                             current_flow = infill_flow * flow_factor
                             if current_flow > hotend_max_flow:
                                 segmentLength = get_points_distance(lastPosition, currentPosition)
-                                new_feedrate = control_flow(hotend_max_flow, newE*flow_factor, segmentLength, d_f)
+                                new_feedrate = control_flow(hotend_max_flow, newE, segmentLength, d_f)
                                 is_old_speed = False
                                 
                                 outPutLine = new_feedrate + outPutLine + "\n"
@@ -640,7 +690,7 @@ if __name__ == '__main__':
                     GRADIENT_THICKNESS = float(input())
                     
                     print('Input INFILL_TYPE choose [0] for SMALL_SEGMENTS and [1] for LINEAR:')
-                    choose_infill_type = int(input())
+                    choose_infill_type = int(input()) #TODO update and find solution inside main program after finding infill type?
                     if choose_infill_type == 0:
                         INFILL_TYPE = InfillType.SMALL_SEGMENTS
                         print('Enable THIN_INNER_CORE ? [y] to enable')
@@ -662,13 +712,13 @@ if __name__ == '__main__':
             start = time.time()   
             # changed out path
             process_gcode(
-                file_path, file_path, INFILL_TYPE, MAX_FLOW, MIN_FLOW, GRADIENT_THICKNESS, GRADIENT_DISCRETIZATION, D_F, THIN_INNER_CORE
+                file_path, file_path, MAX_FLOW, MIN_FLOW, GRADIENT_THICKNESS, GRADIENT_DISCRETIZATION, D_F, THIN_INNER_CORE
             )
             
         else:
             start = time.time()
             process_gcode(
-                INPUT_FILE_NAME, OUTPUT_FILE_NAME, INFILL_TYPE, MAX_FLOW, MIN_FLOW, GRADIENT_THICKNESS, GRADIENT_DISCRETIZATION, D_F, THIN_INNER_CORE
+                INPUT_FILE_NAME, OUTPUT_FILE_NAME, MAX_FLOW, MIN_FLOW, GRADIENT_THICKNESS, GRADIENT_DISCRETIZATION, D_F, THIN_INNER_CORE
             )
             
         print('Time to excecute:',time.time()- start) 
